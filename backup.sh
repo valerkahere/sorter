2153dc23d2205649795b7157a396e0b19ce238bd
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-15 08:46:56 +0100

    Date, Description and Start

---
+++ backup.sh
@@ -0,0 +1,11 @@
+# 15-07-2025
+# take path first
+# save every file in that folder through loop
+# and move it to a separate directory
+
+# ==> just cp every file there.
+
+#!/usr/bin/env bash
+
+printf "\nEnter the path of the files you want to copy:\n"
+read argument
