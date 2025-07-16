1ae446ebbc7fa7223ec0f5aa796a21e2390ce65c
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-16 09:57:45 +0100

    Working base of the script:
     - Fixed find function (-name to -path);
     - Tested and got cp function working;
     - Navigation through path now works in create_files();
     - Added exception-handling for create_files() function.

--- backup.sh
+++ backup.sh
@@ -10,19 +10,26 @@
 # Testing
 
 mkdir TEST
-cd ./TEST
+cd ./TEST 2>/dev/null
 # how many text files to create
 fileNum=1
 files=5
-for i in {1..5}; do
-    touch "file$fileNum.txt"
-    (( ++fileNum ))
-done
-touch ./TEST/
+creating_files() {
+    for i in {1..5}; do # variable expansion does not work in range
+        touch "file$fileNum.txt"
+        (( ++fileNum ))
+    done
+}
 
-printf "\nEnter the path of the dir you want to copy:\n"
+if creating_files; then 
+    printf "\nTest files created in ./TEST folder\n"
+else
+    printf "\nError: could not create files in ./TEST folder\n" >2
+fi
+
+printf "\nEnter the path of the dir you want to copy without slash in the end:\n"
 read source
-if cd "$(find . -type d -name "$source")"; then 
+if cd "$(find . -type d -path "$source")"; then 
     printf "Current directory: "$PWD""
     printf "\nNavigated successfully.\n"
 else 
@@ -32,8 +39,9 @@ fi
 printf "\nEnter the path of the destination dir\n:"
 read destination
 
-# cp --debug -ri --target-directory=DIRECTORY
+# cp --debug -v -ri --target-directory=DIRECTORY 
 # debug: explain how files is copied
 # i: prompt before overwrite
 # r: copy directories recursively
 # v: explain what is being done
+# Tested: function works
