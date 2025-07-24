df0911fdb5522db5283848da6a2d855730a54a11
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-24 13:22:23 +0100

    Final Version 1.0 - All commands on the newline: no semicolons - When found the path of the dir to copy, saves '$PWD' to 'currentdir' - Implemented cp command: goes 2 steps up the current dir, created a folder there "BACKUP", copies from the specified directory "$currentdir"
    Things to change/ to consider:
    - Save "BACKUP" to user directory: /home/'$usr' through '$(whoami)' command?

--- backup.sh
+++ backup.sh
@@ -9,19 +9,21 @@
 
 # Testing
 
-mkdir TEST
-cd ./TEST 2>/dev/null
+mkdir TEST 2>/dev/null
+cd ./TEST 
 # how many text files to create
 fileNum=1
 files=5
 creating_files() {
-    for i in {1..5}; do # variable expansion does not work in range
+    for i in {1..5} # no semicolon needed for multiline
+    do # variable expansion does not work in range
         touch "file$fileNum.txt"
         (( ++fileNum ))
     done
 }
 
-if creating_files; then 
+if creating_files
+then # keywords always on different lines
     printf "\nTest files created in ./TEST folder\n"
 else
     printf "\nError: could not create files in ./TEST folder\n" >2
@@ -29,15 +31,25 @@ fi
 
 printf "\nEnter the path of the dir you want to copy without slash in the end:\n"
 read source
-if cd "$(find . -type d -path "$source")"; then 
+if cd "$(find . -type d -path "$source")"
+then # -D all for debugopts 
     printf "Current directory: "$PWD""
+    currentdir=$PWD
     printf "\nNavigated successfully.\n"
+    # the cd searcher for any dir that's why navigates anyway
 else 
-    printf "\nNo dir with such name.\n" >&2;
+    printf "\nError: %s\n" "could not navigate" >/dev/stderr # changed from >&2 for readability
+    # No need: 2>&1 - Make FD 2 target FD 1's target
+    # Actually need: 1>&2
 fi
 
-printf "\nEnter the path of the destination dir\n:"
-read destination
+cd ../..; mkdir BACKUP; cp --debug -v -ri "$currentdir" "./BACKUP" 
+printf "\nCopied successfully to BACKUP folder in present working directory."
+
+printf "\nError: %s\n" "could not copy\n" >/dev/seterr
+
+
+# mkdir ~/Desktop/Projects/backup; cp "$PWD" --debug -v -ri --target-directory=/home/valerka/Desktop/Projects/backup
 
 # cp --debug -v -ri --target-directory=DIRECTORY 
 # debug: explain how files is copied
