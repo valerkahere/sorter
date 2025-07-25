a9361fd11adcf8195753941d44afc755334ccb4e
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-25 19:08:28 +0100

    Actual folders and more test data created.

--- sorter.sh
+++ sorter.sh
@@ -1,25 +1,18 @@
 
 #!/user/bin/env bash
 
-# mkdir videos
-
-# mv "*.mp4" "*.MOV" --target-directory=./videos --update --strip-trailing-slashes --interactive --verbose
-
-# Или чтобы уменьшить количество ошибок, привязать stdout ls к mv? Use for loop?
-
 # printf "\n\n"
 
 printf "\nWelcome\n"
 
 # Creating a function for error handling
-
-testing() {
+testing() { 
     mkdir 'test' 2>/dev/null
     cd './test'
 
     for (( i=0; i<=5; i++ ))
     do
-        touch "file$i.mp4"
+        touch "file$i.mp4" "sample$i.txt" "photo$i.jpg"
     done
 }
 
@@ -29,8 +22,22 @@ then
 else
     printf "\nError: could not create test folder and files\n" >2
 fi
-printf "\nThis is the present working directory while execeuting the script:\n\n"
+
+printf "\nThis is the present working directory while executing the script:\n\n"
 pwd
 
 printf "\nCreated test files in test folder as follows:\n\n"
 ls
+
+# creating actual folders:
+# docs files, videos, photos
+mkdir 'videos' 'docs' 'photos' 2>/dev/null
+
+# sorting the files based on their extension
+
+
+# mkdir videos
+
+# mv "*.mp4" "*.MOV" --target-directory=./videos --update --strip-trailing-slashes --interactive --verbose
+
+# Или чтобы уменьшить количество ошибок, привязать stdout ls к mv? Use for loop?
