a7f097e6a7a7f17be66cd124473bfec5ae89acbb
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-25 19:24:25 +0100

    Working Version of the script for videos, photos, text files: - Videos: .mp4, .MOV extension - Photos: .jpg, .jpeg extension - Docs: .txt, .docs, .pdf extension

--- sorter.sh
+++ sorter.sh
@@ -12,7 +12,7 @@ testing() {
 
     for (( i=0; i<=5; i++ ))
     do
-        touch "file$i.mp4" "sample$i.txt" "photo$i.jpg"
+        touch "file$i.mp4" "sample$i.txt" "photo$i.jpg" "bruh$i.jpeg" "reading$i.pdf"
     done
 }
 
@@ -23,21 +23,33 @@ else
     printf "\nError: could not create test folder and files\n" >2
 fi
 
-printf "\nThis is the present working directory while executing the script:\n\n"
-pwd
-
 printf "\nCreated test files in test folder as follows:\n\n"
 ls
+
+
+printf "\nThis is the present working directory while executing the script:\n\n"
+pwd
 
 # creating actual folders:
 # docs files, videos, photos
 mkdir 'videos' 'docs' 'photos' 2>/dev/null
 
-# sorting the files based on their extension
+printf "\nSorting the files based on their extension:\n"
+
+printf "Videos:\n"
+# Sorts .mp4, .MOV into ./test/videos dir
+mv *.mp4 *.MOV --target-directory=./videos --update --strip-trailing-slashes --interactive --verbose
+printf "\n"
 
 
-# mkdir videos
+printf "Photos:\n"
+# Sorts .jpg, .jpeg into ./test/photos dir
+mv *.jpg *.jpeg --target-directory=./photos --update --strip-trailing-slashes --interactive --verbose 
+printf "\n"
 
-# mv "*.mp4" "*.MOV" --target-directory=./videos --update --strip-trailing-slashes --interactive --verbose
+printf "Docs:\n"
+# Sorts .txt, .docs, .pdf into ./test/docs dir
+mv *.txt *.docs *.pdf --target-directory=./docs --update --strip-trailing-slashes --interactive --verbose 
+printf "\n"
 
 # Или чтобы уменьшить количество ошибок, привязать stdout ls к mv? Use for loop?
