7ab08f1493b34ea7d3fc28b909545b8e23af8339
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-25 18:44:11 +0100

    First commit. Laid foundation of the script

---
+++ sorter.sh
@@ -0,0 +1,8 @@
+
+#!/user/bin/env bash
+
+# mkdir videos
+
+# mv "*.mp4" "*.MOV" --target-directory=./videos --update --strip-trailing-slashes --interactive --verbose
+
+# Или чтобы уменьшить количество ошибок, привязать stdout ls к mv? Use for loop?
