06c64d1e6ba4b1c7132b2557dfb75098367c074a
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-25 19:00:59 +0100

    Added testing as a function and basic error handling.

--- sorter.sh
+++ sorter.sh
@@ -6,3 +6,31 @@
 # mv "*.mp4" "*.MOV" --target-directory=./videos --update --strip-trailing-slashes --interactive --verbose
 
 # Или чтобы уменьшить количество ошибок, привязать stdout ls к mv? Use for loop?
+
+# printf "\n\n"
+
+printf "\nWelcome\n"
+
+# Creating a function for error handling
+
+testing() {
+    mkdir 'test' 2>/dev/null
+    cd './test'
+
+    for (( i=0; i<=5; i++ ))
+    do
+        touch "file$i.mp4"
+    done
+}
+
+if testing
+then
+    printf "\nTest files with .mp4 extension created in ./test folder\n"
+else
+    printf "\nError: could not create test folder and files\n" >2
+
+printf "\nThis is the present working directory:\n\n"
+pwd
+
+printf "\nCreated test files in test folder as follows:\n\n"
+ls
