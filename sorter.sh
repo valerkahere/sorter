1e42c1807374b9d67f784069ebd425bacd6a7f4a
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-26 16:32:36 +0100

    Abandoned checking if file ext are present:  - Redundant feature  - Fixed comments

--- sorter.sh
+++ sorter.sh
@@ -31,22 +31,7 @@ printf "\nThis is the present working directory while executing the script:\n\n"
 pwd
 
 # creating actual folders:
-# docs files, videos, photos
-mkdir 'videos' 'docs' 'photos' 2>/dev/null
-
-printf "\nTrying to match files first and figure out what extensions are there:"
-# Make FD2 write to where FD1 is writing
-output=$(find . -path "**/*.jpg" -print0 -quit 1>/dev/null 2>&1) 
-
-
-if [ "${my_variable:-'default value'}" = 'default value' ]; then
-    printf "\n .jpg files are not there\n"
-else
-    printf "\n$output"
-    printf "\n => .jpg is there\n"
-fi
-
-# :- operator checks if the value is null, and if it is, sets the "default value"
+# docs, files, videos, photos
 
 printf "\nSorting the files based on their extension:\n"
 
