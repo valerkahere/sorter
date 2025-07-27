d5ee9fad24ba106511445042d8019dc5103a83a6
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-27 15:48:57 +0100

    New function: find  - Finds all of contents of dirs of SOURCE and moves them to ../MOVED
    Other fixes:
     - "MOVED" is now created one level up to prevent issues with find command.
     - Imroved console messages.
     - Adjusted mv commands. Added "audios". Added more extensions.

--- sorter.sh
+++ sorter.sh
@@ -5,13 +5,15 @@
 
 printf "\nWelcome\n"
 
-printf "\nEnter the path of the folder to move:\n"
-read pathtodir
+printf "\nEnter the path of the folder to sort:\n"
+read 'pathtodir'
 cd "$pathtodir"
 
 # Creating a function for error handling
 testing() { 
-    mkdir 'MOVED' 2>/dev/null
+    
+    
+    mkdir '../MOVED' 2>/dev/null
     
 
     
@@ -19,38 +21,48 @@ testing() {
 
 if testing
 then
-    printf "\nCreated test files with .mp4; .txt; .jpeg; .jpg; .pdf extensions created in ./test folder\n"
+    printf "\nCreated "MOVED" dir one level higher than the SOURCE, where SOURCE will be moved to\n"
+
     ls
 else
-    printf "\nError: could not create test folder and files\n" >2
+    printf "\nError: could not create "MOVED" dir\n" >2
 fi
+
+# all contents (files) from dirs in the SOURCE dir will be first recursively moved to "MOVED" dir
+
+find . -type f -exec mv {} ../MOVED \;
 
 
 printf "\nSorting the files based on their extension:\n"
 
 sorting() {
-
+    cd '../MOVED'
     # creating actual folders:
     # docs, files, videos, photos
     
-    mkdir './MOVED/videos' './MOVED/photos' './MOVED/docs'
+    # PATH: /home/user/Desktop/TEST/MOVED
+
+    mkdir 'videos' 'photos' 'docs' 'audios'
     printf "Videos:\n"
     
-    # Sorts .mp4, .MOV into ./test/videos dir
-    mv *.mp4 *.MOV --target-directory=./MOVED/videos --update --strip-trailing-slashes --interactive --verbose
+    mv *.mp4 *.MOV --target-directory=./videos --update --strip-trailing-slashes --interactive --verbose
     printf "\n"
 
 
     printf "Photos:\n"
 
-    # Sorts .jpg, .jpeg into ./test/photos dir
-    mv *.jpg *.jpeg *.png --target-directory=./MOVED/photos --update --strip-trailing-slashes --interactive --verbose 
+    mv *.jpg *.jpeg *.png --target-directory=./photos --update --strip-trailing-slashes --interactive --verbose 
     printf "\n"
 
     printf "Docs:\n"
     
-    # Sorts .txt, .docs, .pdf into ./test/docs dir
-    mv *.txt *.docs *.pdf --target-directory=./MOVED/docs --update --strip-trailing-slashes --interactive --verbose 
+    
+    mv *.txt *.docx *.doc *.zip *.fb2 *.pdf *.xlsx *.pptx --target-directory=./docs --update --strip-trailing-slashes --interactive --verbose 
+    printf "\n"
+
+    printf "Audios:\n"
+    
+    mv *.mp3 *.m4a --target-directory=./audios --update --strip-trailing-slashes --interactive --verbose 
     printf "\n"
 }
 
@@ -63,5 +75,3 @@ fi
 
 printf "\nThis is the present working directory while executing the script:\n"
 pwd
-
-
