eda28f5c7eda907c35d313c8d20a557d2b44544a
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-28 22:29:30 +0100

    Exiftool now sorts everything.

--- sorter.sh
+++ sorter.sh
@@ -53,32 +53,29 @@ sorting() {
     
     # PATH: /home/user/Desktop/TEST/MOVED
 
-    mkdir 'videos' 'photos' 'docs' 'audios' 'other'
-    
-    printf "Videos:\n"
-    
-    mv --target-directory=./videos --update --interactive --verbose -- *.mp4 *.MOV *.mov
-    printf "\n"
+    # Photos
 
+    exiftool '-Directory<DateTimeOriginal' -d '%Y/%m-%B/%d-%m-%Y/photos' -if '$FileTypeExtension =~ /jpe?g|png|webp|gif|bmp|tiff/i' -ext jpg -ext jpeg -ext png -ext webp -ext gif -ext bmp -ext tiff  .
 
-    printf "Photos:\n"
+    exiftool '-Directory<FileModifyDate' -d '%Y/%m-%B/%d-%m-%Y/photos' -if '$FileTypeExtension =~ /jpe?g|png|webp|gif|bmp|tiff/i' -ext jpg -ext jpeg -ext png -ext webp -ext gif -ext bmp -ext tiff .
 
-    mv --target-directory=./photos --update --interactive --verbose -- *.jpg *.jpeg *.png *.webp
-    printf "\n"
+    # Videos
 
-    printf "Docs:\n"
-    
-    mv --target-directory=./docs --update --interactive --verbose -- *.txt *.docx *.doc *.zip *.fb2 *.pdf *.xlsx *.pptx *.ics *.md *.epub
-    printf "\n"
+    exiftool '-Directory<DateTimeOriginal' -d '%Y/%m-%B/%d-%m-%Y/videos' -if '$FileTypeExtension =~ /mp4|mov|avi|mkv|wmv/i' -ext mp4 -ext mov -ext avi -ext mkv -ext wmv .
 
-    printf "Audios:\n"
-    
-    mv --target-directory=./audios --update --interactive --verbose -- *.mp3 *.m4a  
-    printf "\n"
+    exiftool '-Directory<FileModifyDate' -d '%Y/%m-%B/%d-%m-%Y/videos' -if '$FileTypeExtension =~ /mp4|mov/i' -ext mp4 -ext mov -ext avi -ext mkv -ext wmv .
 
+    # Audios
 
-    printf "Other (no extension):\n"
-    find . -maxdepth 1 -type f -exec mv --target-directory=./other --update --interactive --verbose {} \+
+    exiftool '-Directory<DateTimeOriginal' -d '%Y/%m-%B/%d-%m-%Y/audios' -if '$FileTypeExtension =~ /mp3|flac|m4a|wav|ogg/i' -ext mp3 -ext flac -ext m4a -ext wav -ext ogg .
+
+    exiftool '-Directory<FileModifyDate' -d '%Y/%m-%B/%d-%m-%Y/audios' -if '$FileTypeExtension =~ /mp3|flac|m4a|wav|ogg/i' -ext mp3 -ext flac -ext m4a -ext wav -ext ogg . 
+
+    # Docs
+
+    exiftool '-Directory<DateTimeOriginal' -d '%Y/%m-%B/%d-%m-%Y/docs' -if '$FileTypeExtension =~ /pdf|doc|docx|xls|xlsx|ppt|pptx|csv/i' -ext pdf -ext doc -ext docx -ext xls -ext xlsx -ext ppt -ext pptx -ext csv .
+
+    exiftool '-Directory<FileModifyDate' -d '%Y/%m-%B/%d-%m-%Y/docs' -if '$FileTypeExtension =~ /pdf|doc|docx|xls|xlsx|ppt|pptx|csv/i' -ext pdf -ext doc -ext docx -ext xls -ext xlsx -ext ppt -ext pptx -ext csv . 
 }
 
 if sorting
