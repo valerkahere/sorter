f568f47e11d29ffe640fce675639bd4e73783090
Valerii Navalnyi <adultvalerka@gmail.com>
2025-07-31 18:49:01 +0100

    Fixed find command not to match "MOVED" dir. Other changes:  - Improved formatting of the script.
    Problem:
     1) mv "no such directory": not creating audio and docs dirs in the script
    
    Desirable solution:
     1) Create only IF THERE ARE SUCH FILES (find command first)

--- sorter.sh
+++ sorter.sh
@@ -5,7 +5,7 @@
 
 printf "\nWelcome\n"
 
-printf "\nEnter the path of the folder to sort AS IT IS (white spaces are handled automatically):\n"
+printf "\nEnter the path of the (SOURCE) folder to sort AS IT IS (white spaces are handled automatically):\n"
 
 read pathtodir
 
@@ -19,14 +19,13 @@ fi
 
 # Creating a function for error handling
 creating() { 
-    mkdir '../MOVED' 2>/dev/null
+    mkdir './MOVED' 2>/dev/null
 }
 
 if creating
 then
-    printf "\nCreated "MOVED" dir one level higher than the SOURCE, where SOURCE will be moved to\n"
-
-    ls
+    printf "\nCreated "MOVED" folder in the current directory, where all the files from SOURCE will be moved to. \n\n"
+    
 else
     printf "\nError: could not create "MOVED" dir\n" 1>&2
     exit 1
@@ -34,9 +33,10 @@ fi
 
 # all contents (files) from dirs in the SOURCE dir will be first recursively moved to "MOVED" dir
 
-if find . -type f -exec mv {} ../MOVED \;
+# Here, \! - escaping interpretation by shell as script punctutation, same with single quotes around {}. Same protection for semicolon.
+if find . -type f  \! -path "./MOVED/*" -exec mv '{}' ./MOVED \;
 then
-    printf "\nAll regular files are moved to '../MOVED'\n"
+    printf "\nAll regular files are moved to './MOVED'\n"
 else
     printf "\nError: Could not move files" 1>&2
     exit 1
@@ -44,10 +44,15 @@ fi
 
 
 sorting() {
+
+    cd './MOVED'
+    printf "\n\nPWD: $PWD"
+    printf "\n'MOVED' dir contents: $(ls)\n\n"
+    
+
+
     printf "\nSorting the files based on their extension:\n"
 
-    cd '../MOVED'
-    pwd
     # creating actual folders:
     # docs, files, videos, photos
     
@@ -59,7 +64,7 @@ sorting() {
 
     exiftool '-Directory<FileModifyDate' -d '%Y/%m-%B/%d-%m-%Y/photos' -if '$FileTypeExtension =~ /jpe?g|png|webp|gif|bmp|tiff/i' -ext jpg -ext jpeg -ext png -ext webp -ext gif -ext bmp -ext tiff .
 
-    printf "\nSorted Photos.\n"
+    printf "Sorted Photos.\n\n"
 
     # Videos
 
@@ -67,30 +72,27 @@ sorting() {
 
     exiftool '-Directory<FileModifyDate' -d '%Y/%m-%B/%d-%m-%Y/videos' -if '$FileTypeExtension =~ /mp4|mov/i' -ext mp4 -ext mov -ext avi -ext mkv -ext wmv .
 
-    printf "\nSorted Videos.\n"
+    printf "Sorted Videos.\n\n"
 
     # Came to conclusion that I don't want audios and docs sorted in the '%Y/%m-%B/%d-%m-%Y/audios' format
     # Audios
 
     mv --target-directory=./audios --update --interactive --verbose -- *.mp3 *.m4a
 
-    printf "\nSorted Audios.\n"
+    printf "Sorted Audios.\n\n"
 
     # Docs
     mv --target-directory=./docs --update --interactive --verbose -- *.txt *.docx *.doc *.zip *.fb2 *.pdf *.xlsx *.pptx *.ics *.md *.epub
 
-    printf "\nSorted Docs.\n"
+    printf "Sorted Docs.\n\n"
 
    
 }
 
 if sorting
 then
-    printf "\nSorted test files in and moved them to videos, photos and docs folders accordingly.\n"
+    printf "\nSorted files in and moved them to videos, photos and docs folders accordingly.\n"
 else
     printf "\nError: could not move test files.\n" 1>&2
     exit 1
 fi
-
-printf "\nThis is the present working directory while executing the script:\n"
-pwd
