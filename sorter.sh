44f517bf3e8e5fa6ce0da0b76dfeae86715dcec7
Valerii Navalnyi <adultvalerka@gmail.com>
2025-08-01 18:34:36 +0100

    Removed sorter.sh  - It's redundant at the moment.  - Added template function for user-prompting.

--- sorter.sh
+++ sorter.sh
@@ -145,3 +145,14 @@ else
     printf "\nError: could not move test files.\n" 1>&2
     exit 1
 fi
+
+printf "\nDo you want to remove the months folders and their contents? [Y/n]: \n"
+read answer
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+	rm -rf 1.January 2.February 3.March 4.April 5.May 6.June 7.July 8.August 9.September 10.October 11.November 12.December
+	printf "\nFolders deleted as follows: \n\n"
+	ls .
+else
+	printf "\nNothing deleted."
+fi
+printf "\nThe script has finished.\n"
